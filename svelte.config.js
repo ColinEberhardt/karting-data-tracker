21dc133e0bf4aada70c5e75e7dc9e021d8678b5a
Colin Eberhardt <colin.eberhardt@gmail.com>
2025-10-14 18:12:52 +0100

    added github pages build action

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,20 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static'
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/karting-data-tracker' : ''
+    }
+  }
 }
